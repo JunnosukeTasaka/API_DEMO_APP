c5749b4ed963e8b9b14e396c961f396a17a1308d
Junnosuke Tasaka <junnosuke0120@gmail.com>
2020-12-17 11:44:36 +0900

    modify:assosiation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
-class Category < ApplicationRecord
-  validates :name, presence: true
+# frozen_string_literal: true
 
+class Category < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+
+  has_many :ideas, foreign_key: 'category', inverse_of: :category, dependent: :destroy
 end
